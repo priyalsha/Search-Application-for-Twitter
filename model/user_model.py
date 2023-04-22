0dfb7d69d2b331f6240ae009b7b192f87407d480
Bhushan Gupta <cbg27@users.noreply.github.com>
2023-04-21 22:25:31 -0400

    Add files via upload

---
+++ model/user_model.py
@@ -0,0 +1,15 @@
+import mysql.connector
+class user_model():
+    def __init__(self):
+        try:
+            self.con = mysql.connector.connect(user='root', password='password', host='localhost', database='dbms_project', port=3306)
+            print("Some success")
+            self.cur = self.con.cursor(dictionary=True)
+        except:
+            print("Some error")
+    def search_user(self):
+        self.cur.execute("SELECT * FROM twitter_user")
+        results = self.cur.fetchall()
+        for row in results:
+            print(row)
+        return "TABC"
