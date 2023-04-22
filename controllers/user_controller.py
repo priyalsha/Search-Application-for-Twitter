8ad162e03699dcbc4b3207f0038e332685c01a10
Bhushan Gupta <cbg27@users.noreply.github.com>
2023-04-21 22:22:59 -0400

    Add files via upload

--- controllers/user_controller.py
+++ controllers/user_controller.py
@@ -1,4 +1,6 @@
 from app import app
+from model.user_model import user_model
+user_obj = user_model()
 @app.route("/user/tweet")
 def getUserTweets():
-    return "User Tweets"    
+    return user_obj.search_user()
