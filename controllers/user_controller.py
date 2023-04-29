5f395f270211102a65593ed69c82e2bd0e11d14a
rutvikd1205 <130310157+rutvikd1205@users.noreply.github.com>
2023-04-29 09:28:48 -0400

    Add files via upload

--- controllers/user_controller.py
+++ controllers/user_controller.py
@@ -6,17 +6,27 @@ from model.user_model import user_model
 obj = user_model()
 
 @app.route("/user/all")
-def all_users():
+def all_user_model():
     return obj.all_user_model()
-
 
 @app.route("/user/<name>")
 def specific_user(name):
     return obj.get_specific_user(name)
-
 
 @app.route("/search")
 def get_search_user():
     arg1 = request.args['arg1']
     arg2 = request.args['arg2']
     return obj.get_user_search(arg1,arg2)
+
+@app.route("/wildsearch/<name>")
+def wild(name):
+    return obj.wild_search(name)
+
+@app.route("/usercount/<name>")
+def usercount(name):
+    return obj.user_count(name)
+
+@app.route("/top10")
+def top10_users():
+    return obj.top10_users()
