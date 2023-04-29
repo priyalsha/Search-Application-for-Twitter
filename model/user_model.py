08f5ef1837a9578094c946faf5124c58ed8fc975
Bhushan Gupta <cbg27@users.noreply.github.com>
2023-04-29 00:46:40 -0400

    Add files via upload

--- model/user_model.py
+++ model/user_model.py
@@ -1,19 +1,43 @@
+from datetime import datetime, timedelta
 import mysql.connector
 import json
+from flask import make_response, jsonify
+from config.config import dbconfig
+
+
 class user_model():
     def __init__(self):
-        try:
-            self.con = mysql.connector.connect(user='root', password='password', host='localhost', database='dbms_project', port=3306)
-            print("Some success")
-            self.cur = self.con.cursor(dictionary=True)
-        except:
-            print("Some error")
-    def search_user(self):
-        self.cur.execute("SELECT * FROM twitter_user")
-        results = self.cur.fetchall()
-        for row in results:
-            print(row)
-        if len(results)>0:
-            return json.dumps(results)
+        self.con = mysql.connector.connect(host=dbconfig['host'],user=dbconfig['username'],password=dbconfig['password'],database=dbconfig['database'])
+        self.con.autocommit=True
+        self.cur = self.con.cursor(dictionary=True)
+        
+    def all_user_model(self):
+        self.cur.execute("SELECT * FROM users")
+        result = self.cur.fetchall()
+        if len(result)>0:
+            return {"payload":result}
+
         else:
             return "No Data Found"
+    def get_specific_user(self,userid):
+        user = (userid,)
+        self.cur.execute("SELECT * FROM users where name=%s",user)
+        result = self.cur.fetchall()
+        if len(result)>0:
+            return {"payload":result}
+
+        else:
+            return "No Data Found"
+        
+    def get_user_search(self,name, location):
+        user = (name,location,)
+        self.cur.execute("SELECT * FROM users where name=%s and location=%s",user)
+        result = self.cur.fetchall()
+        if len(result)>0:
+            return {"payload":result}
+
+        else:
+            return "No Data Found"
+        
+
+        
