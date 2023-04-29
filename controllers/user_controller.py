263e13c8a95e58bdd9d4aec14ac0c3d129db3b50
Bhushan Gupta <cbg27@users.noreply.github.com>
2023-04-29 00:45:55 -0400

    Add files via upload

--- controllers/user_controller.py
+++ controllers/user_controller.py
@@ -1,6 +1,22 @@
+import imp
+import flask
+from flask import request
 from app import app
 from model.user_model import user_model
-user_obj = user_model()
-@app.route("/user/tweet")
-def getUserTweets():
-    return user_obj.search_user()
+obj = user_model()
+
+@app.route("/user/all")
+def all_users():
+    return obj.all_user_model()
+
+
+@app.route("/user/<name>")
+def specific_user(name):
+    return obj.get_specific_user(name)
+
+
+@app.route("/search")
+def get_search_user():
+    arg1 = request.args['arg1']
+    arg2 = request.args['arg2']
+    return obj.get_user_search(arg1,arg2)
