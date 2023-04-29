6ed88c5bdd3ad5692173df70812d572b7f7474da
Bhushan Gupta <cbg27@users.noreply.github.com>
2023-04-29 00:47:33 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -2,18 +2,6 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/")
-def hellWorld():
-    return "hello world"
-
-@app.route("/home")
-def Home():
-    return "This is home page"
-
-@app.route("/myRoute")
-def My():
-    return "This is my page"    
-
 try:
     from controllers import *
 except Exception as e:
